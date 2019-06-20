26e7fefa712dfc13641a2fe4aeda09d45900e58c
Matheus Fornazieri <47992677+matheusf0@users.noreply.github.com>
2019-06-20 15:55:04 -0300

    Add files via upload

---
+++ FedoraFastSetup.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+#System update
+sudo dnf update -y
+sudo dnf upgrade -y
+
+#packages
+sudo dnf install snapd
+sudo ln -s /var/lib/snapd/snap /snap
+
+#dev tools
+sudo snap install --classic code
+sudo snap install pycharm-community --classic
+sudo dnf install git
+sudo snap install opera
+sudo dnf install pdadmin3 -y
+sudo dnf install docker -y
+
+#players
+sudo snap install spotify 
+sudo dnf install vlc
+
+#Tools
+sudo snap install ktube-media-downloader
+sudo dnf install veracrypt -y
+sudo dnf install chromium -y
+sudo dnf install youtube-dl -y
+sudo dnf install gparted -y
+sudo dnf install obs-studio -y
+
+#gaming
+sudo dnf install steam -y
+
